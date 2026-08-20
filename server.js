41a589b38f1aa0d11cd397f3af5b4f2a38ff2a79
mini <2025.saudamini@isu.ac.in>
2026-08-20 13:15:50 +0530

    Prepare API for Render deployment

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const menuRoutes = require("./routes/menuRoutes");
 require("dotenv").config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 app.use("/", authRoutes);
@@ -22,8 +25,8 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
     })
     .catch((error) => {
