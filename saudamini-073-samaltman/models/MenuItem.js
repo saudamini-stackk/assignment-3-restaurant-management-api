1749b50ccc02f0887191a97125aeb53036e6d6a2
saudamini nayak <2025.saudamini@isu.ac.in>
2026-08-21 11:20:28 +0530

    Add files via upload

---
+++ saudamini-073-samaltman/models/MenuItem.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String,
+            required: true
+        },
+
+        price: {
+            type: Number,
+            required: true
+        },
+
+        category: {
+            type: String,
+            required: true
+        },
+
+        restaurant: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Restaurant",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
